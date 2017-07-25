05b7cd5838a4f094aef6a541f91dda732194bbc1
Jesus Villegas <jesus_villegas@intuit.com>
2017-07-25 09:31:40 -0700

    Works yolo swag

---
+++ src/Solution12.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Solution12 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        long curCount = 0;
+        while(true) {
+            curCount += i++;
+            ArrayList<Long> ret = getFactors(curCount);
+            if(ret.size() > 500) {
+                System.out.println("First number with over 500 divisors is: " + curCount);
+                return;
+            }
+        }
+    }
+
+    private static ArrayList<Long> getFactors(long num) {
+        ArrayList<Long> retList = new ArrayList<>();
+        Long end = Long.MAX_VALUE;
+        for(int i = 1; i < end; i++)
+            if(num % i == 0) {
+                retList.add((long) i);
+                end = num / i;
+                retList.add(end);
+            }
+        return retList;
+    }
+}
