6a780f36271624aff373542050e242a11b5988c8
Jesus Villegas <jesus_villegas@intuit.com>
2017-07-26 12:53:34 -0700

    Pretty

--- src/Solution31.java
+++ src/Solution31.java
@@ -3,9 +3,15 @@ import java.util.ArrayList;
 public class Solution31 {
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
         int[] coins = {1, 2, 5, 10, 20, 50, 100, 200};
         long totalPermutations = getPermutations(coins);
+
+        long endTime = System.currentTimeMillis();
+        float duration = ((endTime - startTime)/ (float)1000);  //divide by 1000000 to get milliseconds.
         System.out.println("Final way to make the highest coin is: " + totalPermutations);
+        System.out.println("Execution took these many seconds: " + duration);
     }
 
     private static long getPermutations(int[] coins) {
@@ -19,13 +25,13 @@ public class Solution31 {
     private static long getPermutationsHelper(int[] coins, int startingIndex, int runningTotal, ArrayList<Integer> curCoins) {
         long count = 0;
         runningTotal += coins[startingIndex];
-        curCoins.add(coins[startingIndex]);
+//        curCoins.add(coins[startingIndex]);
 
         if (runningTotal == coins[coins.length - 1]) {
-            System.out.print("Found a match with coins: ");
+            /*System.out.print("Found a match with coins: ");
             for (int i : curCoins)
                 System.out.print(i + " ");
-            System.out.println();
+            System.out.println();*/
             return 1;
         }
         if (runningTotal > coins[coins.length - 1]) {
@@ -34,7 +40,7 @@ public class Solution31 {
 
         for (int i = startingIndex; i < coins.length; i++) {
             count += getPermutationsHelper(coins, i, runningTotal, curCoins);
-            curCoins.remove(curCoins.size() - 1);
+//            curCoins.remove(curCoins.size() - 1);
         }
         return count;
     }
